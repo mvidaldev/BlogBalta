8848261694ca33b72d51fc867000ab3cbd38fce8
Marcos Vidal <marcosvidal@outlook.com>
2022-12-06 13:15:23 -0300

    Added passwordhash and saved it to the database

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -4,7 +4,9 @@ using Blog.Models;
 using Blog.Services;
 using Blog.ViewModel;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using SecureIdentity.Password;
 
 namespace Blog.Controllers;
 
@@ -15,17 +17,45 @@ public class AccountController :ControllerBase
     [HttpPost("v1/accounts/")]
     public async Task<IActionResult> Post([FromBody]RegisterViewModel model,[FromServices]BlogDataContext context)
     {
-        if (ModelState.IsValid)
+        if (!ModelState.IsValid)
             return BadRequest(new ResultViewModel<string>(ModelState.GetErrors()));
-        var user = new User()
+        var user = new User
         {
             Name = model.Name,
             Email = model.Email,
             Slug = model.Email.Replace("@", "-").Replace(".", "-")
 
         };
-        return Ok();
+
+        var password = PasswordGenerator.Generate(25);
+        user.PasswordHash = PasswordHasher.Hash(password);
+
+        try
+        {
+            await context.Users.AddAsync(user);
+            await context.SaveChangesAsync();
+
+            return Ok(new ResultViewModel<dynamic>(new
+            {
+                user = user.Email, password
+            }));
+        }
+        catch (DbUpdateException)
+        {
+            return StatusCode(400, new ResultViewModel<string>("0x599 - Email já cadastrado"));
+        }
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<dynamic>("0x020 - Falha interna do servidor."));
+        }
+
+
+
+
+
+
     }
+    
     
 
 
--- ViewModel/RegisterViewModel.cs
+++ ViewModel/RegisterViewModel.cs
@@ -5,10 +5,10 @@ namespace Blog.ViewModel;
 public class RegisterViewModel
 {
     [Required(ErrorMessage = "Name is mandatory")]
-    public string Name { get; set; }
+    public required string Name { get; set; }
 
     [Required(ErrorMessage = "Email is Mandatory")]
     [EmailAddress(ErrorMessage = "Invalid email.")]
-    public string Email { get; set; }
+    public required string Email { get; set; }
     
 }
